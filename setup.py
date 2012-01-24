3c74bc53170af680ef8b645948160c26706b2bf1
Álvaro Arranz García <aarranz@conwet.com>
2012-01-24 12:28:41 +0100

    Add initial setup.py code

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import os
+from setuptools import setup, findall
+from distutils.command.install import INSTALL_SCHEMES
+
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+
+packages, data_files = [], []
+for dirpath, dirnames, filenames in os.walk('wirecloud_pubsub'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' not in filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+# Dynamically calculate the version based on wirecloud_pubsub.VERSION.
+version = __import__('wirecloud_pubsub').VERSION
+
+setup(
+    name='wirecloud-pubsub',
+    version='0.1',
+    description='Wirecloud plugin providing PubSub support.',
+    long_description=open(os.path.join(ROOT, 'README.rst')).read(),
+    author='CoNWeT Lab',
+    author_email='wirecloud-pubsub@conwet.com',
+    url='http://github.com/conwetlab/wirecloud-pubsub',
+    license='EUPL 1.1',
+    packages=('wirecloud_pubsub',),
+    data_files=data_files,
+    install_requires=('Django',),
+    tests_require=('Django',),
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: European Union Public Licence 1.1 (EUPL 1.1)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    )
+)
