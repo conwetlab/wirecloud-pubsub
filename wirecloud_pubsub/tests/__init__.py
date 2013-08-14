74f0f75c1c1cc087e18ec68672dcdb6ca8d4e718
Álvaro Arranz <aarranz@conwet.com>
2013-08-14 17:19:31 +0200

    Fix wirecloud tests

--- wirecloud_pubsub/tests/__init__.py
+++ wirecloud_pubsub/tests/__init__.py
@@ -1,3 +1,1 @@
-from wirecloud.commons.test import build_selenium_test_cases
-
-build_selenium_test_cases(('wirecloud_pubsub.tests.basic.BasicTestCase',), locals())
+from wirecloud_pubsub.tests.basic import *
--- wirecloud_pubsub/tests/basic.py
+++ wirecloud_pubsub/tests/basic.py
@@ -1,6 +1,6 @@
-from wirecloud.commons.test import iwidget_context, WirecloudSeleniumTestCase
+from wirecloud.commons.utils.testcases import wirecloud_selenium_test_case, WirecloudSeleniumTestCase
 
-
+@wirecloud_selenium_test_case
 class BasicTestCase(WirecloudSeleniumTestCase):
 
     def test_silbops_available_to_widgets(self):
@@ -10,7 +10,7 @@ class BasicTestCase(WirecloudSeleniumTestCase):
         resource = self.add_packaged_resource_to_catalogue('Wirecloud_silbops-test-widget_1.0.wgt', 'Wirecloud SilboPS API test widget')
         iwidget = self.instantiate(resource)
 
-        with iwidget_context(self.driver, iwidget['id']):
+        with iwidget:
             body = self.driver.find_element_by_tag_name('body')
             self.assertEqual(body.text, 'Success')
     test_silbops_available_to_widgets.tags = ('fiware-ut-10',)
